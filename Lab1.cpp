778c5d5fb8e25900b0fa35cb85c27ab5617875fb
zpope1 <zpope@umich.edu>
2025-09-09 23:36:37 -0400

    Add files via upload

--- Lab1.cpp
+++ Lab1.cpp
@@ -3,26 +3,27 @@ using namespace std;
 
 // Program to convert a Fahrenheit temperature to Celsius
 
-int main()
-{
+int main() {
     // declaration of variables
-    int cel, fah;
+    double cel, fah;
 
     // print header
     cout << "Lab 1\nTemperature Conversion\nFahrenheit to Cesius\n\n";
 
     // collection of user input
     cout << "Enter Fahrenheit Temperature: ";
-    cin << fah;
+    cin >> fah;
 
     // calculation of temperature
-    cel = fah - 32 * 5 / 9;
+    cel = (fah - 32) * 5 / 9;
+
 
     // formatted output of results
-    cout << endl << fah << " degrees Fahrenheit is equal to "
+
+    cout << endl << fah << " degrees Fahrenheit is equal to ";
+    cout.setf(ios::fixed, ios::floatfield);
     cout.precision(2);
-    cout.setf(ios::fixed);
     cout << cel << " degrees Celsius.\n";
 
-    return 0;
+        return 0;
 }
